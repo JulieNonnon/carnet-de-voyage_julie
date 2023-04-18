857eb666ddc312fb37c4fdcba9f873c0a0efcf5b
JulieNonnon <julie.nonnon@gmail.com>
2023-04-18 19:51:34 +0200

    feature: home page created, to be fixed later

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeLatestArticleComponent } from './components/home-latest-article/home-latest-article.component';
 import { HomeOtherArticlesComponent } from './components/home-other-articles/home-other-articles.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 @NgModule({
@@ -15,7 +16,9 @@ import { HomeOtherArticlesComponent } from './components/home-other-articles/hom
     HeaderComponent,
     FooterComponent,
     HomeLatestArticleComponent,
-    HomeOtherArticlesComponent
+    HomeOtherArticlesComponent,
+    HomeComponent
+    
   ],
   imports: [
     BrowserModule,
